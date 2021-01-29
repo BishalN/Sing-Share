6038987c53151f5279331d26223d1b22953bb196
Bishal Neupane <neupanebishal07@gmail.com>
2021-01-29 16:44:01 +0545

    Finishing work on the profile of the user

--- frontend/src/pages/[username].tsx
+++ frontend/src/pages/[username].tsx
@@ -9,8 +9,61 @@ import {
 } from '@chakra-ui/react';
 import React from 'react';
 import { Layout } from '../components/Layout';
+import {
+  AiFillPlayCircle,
+  AiOutlineComment,
+  AiOutlineHeart,
+} from 'react-icons/ai';
 
 interface usernameProps {}
+
+const AudioPlayer = (porps) => {
+  return (
+    <>
+      <Box
+        as='div'
+        alignSelf='start'
+        mt='4'
+        bg='black'
+        color='white'
+        rounded='xl'
+        boxShadow='md'
+        _hover={{ boxShadow: 'lg' }}
+      >
+        <Flex alignItems='center' p={3} direction='row'>
+          <AiFillPlayCircle size={70} />
+          <Box>
+            <Text ml='3'>
+              This song is dedicated to my friend{' '}
+              <Badge
+                rounded='lg'
+                bg='primaryColor'
+                color='whitesmoke'
+                textTransform='lowercase'
+              >
+                #memories
+              </Badge>
+            </Text>
+          </Box>
+          <Box ml='3' display='flex'>
+            <Box>
+              <AiOutlineHeart size={35} />
+              <Badge bg='primaryColor' rounded='sm' color='white'>
+                1000
+              </Badge>
+            </Box>
+            <Box ml={3}>
+              <AiOutlineComment size={35} />
+              <Badge bg='primaryColor' rounded='sm' color='white'>
+                50
+              </Badge>
+            </Box>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+};
 
 const Username: React.FC<usernameProps> = ({}) => {
   return (
@@ -38,9 +91,13 @@ const Username: React.FC<usernameProps> = ({}) => {
           This is segun adebayo designer and developer of chakra ui
         </Text>
         <Divider color='grey.700' mt='3' />
-        <Text fontWeight='medium' fontSize='xl'>
-          Total 5 recordings
+        <Text fontWeight='normal' fontSize='xl'>
+          5 songs recorded
         </Text>
+        <Text alignSelf='start' mt='4' fontWeight='bold' fontSize='xl'>
+          Latest Recordings:
+        </Text>
+        <AudioPlayer />
       </Flex>
     </Layout>
   );
