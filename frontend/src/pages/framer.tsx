bb7ae42fc7197d66130bcef6c7160a3ff4dce7b4
Bishal Neupane <neupanebishal07@gmail.com>
2021-05-05 08:29:21 +0545

    animation

--- frontend/src/pages/framer.tsx
+++ frontend/src/pages/framer.tsx
@@ -4,7 +4,8 @@ import { motion } from 'framer-motion';
 const Animate = () => {
   return (
     <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scale: 0.95 }}>
-      <h1>this is the animation heading</h1>;
+      <h1>this is the animation heading</h1>
+      <h1>hello of deals out here</h1>
     </motion.div>
   );
 };
