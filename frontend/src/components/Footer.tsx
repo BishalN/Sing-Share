148b1106e199c9051cb04343365722b7480e8194
Bishal Neupane <neupanebishal07@gmail.com>
2021-08-17 15:38:42 +0545

    mes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -15,6 +15,9 @@ export const Footer: React.FC<FooterProps> = ({}) => {
         color='white'
         background='gray.800'
       >
+        <Text letterSpacing='5px' textAlign='center'>
+          All the data used here are just for demo purpose
+        </Text>
         <Text letterSpacing='5px' fontWeight='bold' textAlign='center'>
           &copy;2021 Sing&Share
         </Text>
