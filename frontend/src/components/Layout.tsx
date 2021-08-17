811c9711de1c65ace31ff69adc612786a9136468
Bishal Neupane <neupanebishal07@gmail.com>
2021-08-17 15:37:03 +0545

    remove header alert

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -16,29 +16,8 @@ import Head from 'next/head';
 interface LayoutProps {}
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [showAlert, setShowAlert] = useState(true);
   return (
     <>
-      {showAlert ? (
-        <Alert status='info'>
-          <AlertIcon />
-          <Box flex='1'>
-            <AlertTitle>Under Development!</AlertTitle>
-            <AlertDescription display='block'>
-              This site is under development and using dummy data for testing !!
-            </AlertDescription>
-          </Box>
-          <CloseButton
-            position='absolute'
-            right='8px'
-            top='8px'
-            onClick={() => setShowAlert(false)}
-          />
-        </Alert>
-      ) : (
-        ''
-      )}
-
       <NavBar />
       <Container>{children}</Container>
       <Footer />
